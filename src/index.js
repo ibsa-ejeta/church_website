7a9c90d6514ea119772bef3e246ac6eefefdfb28
ibsa-ejeta <ibsa.ej@gmail.com>
2022-08-04 22:38:22 -0400

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 
 reactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
